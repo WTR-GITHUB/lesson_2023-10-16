498581a30d9ad2cc4051ba7012dc6972bbf3f161
Saulius Anusas <saulius.anusas@gmail.com>
2023-10-16 19:25:21 +0300

    Add aditional

---
+++ task1.py
@@ -0,0 +1,6 @@
+dictionary_random = {}
+i = 0
+while i < 10:
+    dictionary_random[i+1] = (i+2)**2
+    i += 1
+print(dictionary_random)
---
+++ task2.py
@@ -0,0 +1,8 @@
+# Create a pin code cracker. Let's say pin code consists of 4 random digits. You can store the value in variable.
+# Then create a loop going through all possible combinations until you find the one you entered.
+
+pin = 9234
+i = 0
+while i != pin:
+    i += 1
+print(i)    
